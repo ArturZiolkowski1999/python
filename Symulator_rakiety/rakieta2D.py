a16d856c8e4d741bd7b5844f5f3636a082448e90
ziolko <aaartiii99@gmail.com>
2020-12-13 20:06:44 +0100

    dorzucone

--- Symulator_rakiety/rakieta2D.py
+++ Symulator_rakiety/rakieta2D.py
@@ -1,18 +1,16 @@
 from pylab import *
 import matplotlib.animation as animation
-import math
 
 # stale
 M0 = 1 #kg masa rakiety bez paliwa
 MP = 10 # masa rakiety z paliwem
 g = 9.81 #m/s**2 przysp ziemskie
-K0 = 30 #m/s**2 ciag silnika poczatkowy
 s = 0.01 # stala zaniku eksponenty ciagu silnika - model eksponencjalny
 c = 3000.0 # v spalin
 h = 0.05 #s skok
 b = 0.2 # stala oporow powietrza
 l = 2 # wysokosc rakiety
-simulation_time = 80 #s czas symulacji
+simulation_time = 60 #s czas symulacji
 number_of_steps = 1000 # liczba krokow
 xrange_right = 10000
 xrange_left = -100
@@ -20,35 +18,35 @@ yrange = 8000
 
 
 # funkcja przyspieszenia wspolrzednych uogulnionych z pomoca rownaia lagrangea 2
-def f_ax(t,x,y,fi,vx,vy,vfi, m, m_prim, k):
+def f_ax(t,x,y,fi,vx,vy,vfi, m, m_prim):
     if y >= 0:
-        return  s * c * (1.0 - (M0 / m)) * cos(fi)
+        return s * c * (1.0 - (M0 / m)) * cos(fi)
     else:
         return 0
 
-def f_ay(t,x,y,fi,vx,vy,vfi, m, m_prim, k):
+def f_ay(t,x,y,fi,vx,vy,vfi, m, m_prim):
     if y >= 0:
         return -g + s * c * (1.0 - (M0/m)) * sin(fi)
     else:
         return 0
 
-def f_afi(t,x,y,fi,vx,vy,vfi,m, m_prim, k):
+def f_afi(t,x,y,fi,vx,vy,vfi,m, m_prim):
     return 0
 
 # funkcja definiujaca przedkosc otrzymana z rownania lagrangea 2
-def f_vx(t,x,y,fi,vx,vy,vfi,m, m_prim, k):
+def f_vx(t,x,y,fi,vx,vy,vfi,m, m_prim):
     if y >= 0:
         return vx
     else:
         return 0
 
-def f_vy(t,x,y,fi,vx,vy,vfi,m, m_prim, k):
+def f_vy(t,x,y,fi,vx,vy,vfi,m, m_prim):
     if y >= 0:
         return vy
     else:
         return 0
 
-def f_vfi(t,x,y,fi,vx,vy,vfi, m, m_prim, k):
+def f_vfi(t,x,y,fi,vx,vy,vfi, m, m_prim):
     return vfi
 
 # funkcje do wyliczania masy i jej pochodnej oraz ciagu
@@ -59,8 +57,6 @@ def f_m(t):
 def f_dm_dt(t):
     return -s*(MP * exp(-s * t))
 
-def f_k(t):
-    return 0
 
 
 
@@ -69,7 +65,7 @@ x0 = 0 #m polozenie poczatkowe
 vx0 = 0 #m/s predkosc poczatkowa
 y0 = 0
 vy0 = 0
-fi0 = pi/4
+fi0 = pi/3
 vfi0 = 0
 
 
@@ -82,7 +78,6 @@ vy = [vy0]
 fi = [fi0]
 vfi = [vfi0]
 #definicja ciagu i masy
-k = [K0]
 m = [M0 + MP]
 m_prim = [f_dm_dt(0)]
 
@@ -93,46 +88,42 @@ for i in range(len(t) - 1):
     #if (y[i] >= 0):
         #wspolrzedna x
         (t, x, y, fi, vx, vy, vfi)
-        K1ax = h * f_ax(t[i], x[i], y[i], fi[i], vx[i], vy[i], vfi[i], m[i], m_prim[i], k[i])
-        K1vx = h * f_vx(t[i], x[i], y[i], fi[i], vx[i], vy[i], vfi[i], m[i], m_prim[i], k[i])
-        K1ay = h * f_ay(t[i], x[i], y[i], fi[i], vx[i], vy[i], vfi[i], m[i], m_prim[i], k[i])
-        K1vy = h * f_vy(t[i], x[i], y[i], fi[i], vx[i], vy[i], vfi[i], m[i], m_prim[i], k[i])
-        K1afi = h * f_afi(t[i], x[i], y[i], fi[i], vx[i], vy[i], vfi[i], m[i], m_prim[i], k[i])
-        K1vfi = h * f_vfi(t[i], x[i], y[i], fi[i], vx[i], vy[i], vfi[i], m[i], m_prim[i], k[i])
+        K1ax = h * f_ax(t[i], x[i], y[i], fi[i], vx[i], vy[i], vfi[i], m[i], m_prim[i])
+        K1vx = h * f_vx(t[i], x[i], y[i], fi[i], vx[i], vy[i], vfi[i], m[i], m_prim[i])
+        K1ay = h * f_ay(t[i], x[i], y[i], fi[i], vx[i], vy[i], vfi[i], m[i], m_prim[i])
+        K1vy = h * f_vy(t[i], x[i], y[i], fi[i], vx[i], vy[i], vfi[i], m[i], m_prim[i])
+        K1afi = h * f_afi(t[i], x[i], y[i], fi[i], vx[i], vy[i], vfi[i], m[i], m_prim[i])
+        K1vfi = h * f_vfi(t[i], x[i], y[i], fi[i], vx[i], vy[i], vfi[i], m[i], m_prim[i])
 
-        K2ax = h * f_ax(t[i] + 0.5 * h, x[i] + K1ax * 0.5, y[i] + K1ay * 0.5, fi[i] + K1afi * 0.5, vx[i] + K1vx * 0.5, vy[i] + K1vy * 0.5, vfi[i] + K1vfi * 0.5, m[i], m_prim[i], k[i])
-        K2vx = h * f_vx(t[i] + 0.5 * h, x[i] + K1ax * 0.5, y[i] + K1ay * 0.5, fi[i] + K1afi * 0.5, vx[i] + K1vx * 0.5, vy[i] + K1vy * 0.5, vfi[i] + K1vfi * 0.5, m[i], m_prim[i], k[i])
-        K2ay = h * f_ay(t[i] + 0.5 * h, x[i] + K1ax * 0.5, y[i] + K1ay * 0.5, fi[i] + K1afi * 0.5, vx[i] + K1vx * 0.5, vy[i] + K1vy * 0.5, vfi[i] + K1vfi * 0.5, m[i], m_prim[i], k[i])
-        K2vy = h * f_vy(t[i] + 0.5 * h, x[i] + K1ax * 0.5, y[i] + K1ay * 0.5, fi[i] + K1afi * 0.5, vx[i] + K1vx * 0.5, vy[i] + K1vy * 0.5, vfi[i] + K1vfi * 0.5, m[i], m_prim[i], k[i])
-        K2afi = h * f_afi(t[i] + 0.5 * h, x[i] + K1ax * 0.5, y[i] + K1ay * 0.5, fi[i] + K1afi * 0.5, vx[i] + K1vx * 0.5, vy[i] + K1vy * 0.5, vfi[i] + K1vfi * 0.5, m[i], m_prim[i], k[i])
-        K2vfi = h * f_vfi(t[i] + 0.5 * h, x[i] + K1ax * 0.5, y[i] + K1ay * 0.5, fi[i] + K1afi * 0.5, vx[i] + K1vx * 0.5, vy[i] + K1vy * 0.5, vfi[i] + K1vfi * 0.5, m[i], m_prim[i], k[i])
+        K2ax = h * f_ax(t[i] + 0.5 * h, x[i] + K1ax * 0.5, y[i] + K1ay * 0.5, fi[i] + K1afi * 0.5, vx[i] + K1vx * 0.5, vy[i] + K1vy * 0.5, vfi[i] + K1vfi * 0.5, m[i], m_prim[i])
+        K2vx = h * f_vx(t[i] + 0.5 * h, x[i] + K1ax * 0.5, y[i] + K1ay * 0.5, fi[i] + K1afi * 0.5, vx[i] + K1vx * 0.5, vy[i] + K1vy * 0.5, vfi[i] + K1vfi * 0.5, m[i], m_prim[i])
+        K2ay = h * f_ay(t[i] + 0.5 * h, x[i] + K1ax * 0.5, y[i] + K1ay * 0.5, fi[i] + K1afi * 0.5, vx[i] + K1vx * 0.5, vy[i] + K1vy * 0.5, vfi[i] + K1vfi * 0.5, m[i], m_prim[i])
+        K2vy = h * f_vy(t[i] + 0.5 * h, x[i] + K1ax * 0.5, y[i] + K1ay * 0.5, fi[i] + K1afi * 0.5, vx[i] + K1vx * 0.5, vy[i] + K1vy * 0.5, vfi[i] + K1vfi * 0.5, m[i], m_prim[i])
+        K2afi = h * f_afi(t[i] + 0.5 * h, x[i] + K1ax * 0.5, y[i] + K1ay * 0.5, fi[i] + K1afi * 0.5, vx[i] + K1vx * 0.5, vy[i] + K1vy * 0.5, vfi[i] + K1vfi * 0.5, m[i], m_prim[i])
+        K2vfi = h * f_vfi(t[i] + 0.5 * h, x[i] + K1ax * 0.5, y[i] + K1ay * 0.5, fi[i] + K1afi * 0.5, vx[i] + K1vx * 0.5, vy[i] + K1vy * 0.5, vfi[i] + K1vfi * 0.5, m[i], m_prim[i])
 
-        K3ax = h * f_ax(t[i] + 0.5 * h, x[i] + K2ax * 0.5, y[i] + K2ay * 0.5, fi[i] + K2afi * 0.5, vx[i] + K2vx * 0.5, vy[i] + K2vy * 0.5, vfi[i] + K2vfi * 0.5, m[i], m_prim[i], k[i])
-        K3vx = h * f_vx(t[i] + 0.5 * h, x[i] + K2ax * 0.5, y[i] + K2ay * 0.5, fi[i] + K2afi * 0.5, vx[i] + K2vx * 0.5, vy[i] + K2vy * 0.5, vfi[i] + K2vfi * 0.5, m[i], m_prim[i], k[i])
-        K3ay = h * f_ay(t[i] + 0.5 * h, x[i] + K2ax * 0.5, y[i] + K2ay * 0.5, fi[i] + K2afi * 0.5, vx[i] + K2vx * 0.5, vy[i] + K2vy * 0.5, vfi[i] + K2vfi * 0.5, m[i], m_prim[i], k[i])
-        K3vy = h * f_vy(t[i] + 0.5 * h, x[i] + K2ax * 0.5, y[i] + K2ay * 0.5, fi[i] + K2afi * 0.5, vx[i] + K2vx * 0.5, vy[i] + K2vy * 0.5, vfi[i] + K2vfi * 0.5, m[i], m_prim[i], k[i])
-        K3afi = h * f_afi(t[i] + 0.5 * h, x[i] + K2ax * 0.5, y[i] + K2ay * 0.5, fi[i] + K2afi * 0.5, vx[i] + K2vx * 0.5, vy[i] + K2vy * 0.5, vfi[i] + K2vfi * 0.5, m[i], m_prim[i], k[i])
-        K3vfi = h * f_vfi(t[i] + 0.5 * h, x[i] + K2ax * 0.5, y[i] + K2ay * 0.5, fi[i] + K2afi * 0.5, vx[i] + K2vx * 0.5, vy[i] + K2vy * 0.5, vfi[i] + K2vfi * 0.5, m[i], m_prim[i], k[i])
+        K3ax = h * f_ax(t[i] + 0.5 * h, x[i] + K2ax * 0.5, y[i] + K2ay * 0.5, fi[i] + K2afi * 0.5, vx[i] + K2vx * 0.5, vy[i] + K2vy * 0.5, vfi[i] + K2vfi * 0.5, m[i], m_prim[i])
+        K3vx = h * f_vx(t[i] + 0.5 * h, x[i] + K2ax * 0.5, y[i] + K2ay * 0.5, fi[i] + K2afi * 0.5, vx[i] + K2vx * 0.5, vy[i] + K2vy * 0.5, vfi[i] + K2vfi * 0.5, m[i], m_prim[i])
+        K3ay = h * f_ay(t[i] + 0.5 * h, x[i] + K2ax * 0.5, y[i] + K2ay * 0.5, fi[i] + K2afi * 0.5, vx[i] + K2vx * 0.5, vy[i] + K2vy * 0.5, vfi[i] + K2vfi * 0.5, m[i], m_prim[i])
+        K3vy = h * f_vy(t[i] + 0.5 * h, x[i] + K2ax * 0.5, y[i] + K2ay * 0.5, fi[i] + K2afi * 0.5, vx[i] + K2vx * 0.5, vy[i] + K2vy * 0.5, vfi[i] + K2vfi * 0.5, m[i], m_prim[i])
+        K3afi = h * f_afi(t[i] + 0.5 * h, x[i] + K2ax * 0.5, y[i] + K2ay * 0.5, fi[i] + K2afi * 0.5, vx[i] + K2vx * 0.5, vy[i] + K2vy * 0.5, vfi[i] + K2vfi * 0.5, m[i], m_prim[i])
+        K3vfi = h * f_vfi(t[i] + 0.5 * h, x[i] + K2ax * 0.5, y[i] + K2ay * 0.5, fi[i] + K2afi * 0.5, vx[i] + K2vx * 0.5, vy[i] + K2vy * 0.5, vfi[i] + K2vfi * 0.5, m[i], m_prim[i])
 
-        K4ax = h * f_ax(t[i] + h, x[i] + K3ax, y[i] + K3ay, fi[i] + K3afi, vx[i] + K3vx, vy[i] + K3vy, vfi[i] + K3vfi, m[i], m_prim[i], k[i])
-        K4vx = h * f_vx(t[i] + h, x[i] + K3ax, y[i] + K3ay, fi[i] + K3afi, vx[i] + K3vx, vy[i] + K3vy, vfi[i] + K3vfi, m[i], m_prim[i], k[i])
-        K4ay = h * f_ay(t[i] + h, x[i] + K3ax, y[i] + K3ay, fi[i] + K3afi, vx[i] + K3vx, vy[i] + K3vy, vfi[i] + K3vfi, m[i], m_prim[i], k[i])
-        K4vy = h * f_vy(t[i] + h, x[i] + K3ax, y[i] + K3ay, fi[i] + K3afi, vx[i] + K3vx, vy[i] + K3vy, vfi[i] + K3vfi, m[i], m_prim[i], k[i])
-        K4afi = h * f_afi(t[i] + h, x[i] + K3ax, y[i] + K3ay, fi[i] + K3afi, vx[i] + K3vx, vy[i] + K3vy, vfi[i] + K3vfi, m[i], m_prim[i], k[i])
-        K4vfi = h * f_vfi(t[i] + h, x[i] + K3ax, y[i] + K3ay, fi[i] + K3afi, vx[i] + K3vx, vy[i] + K3vy, vfi[i] + K3vfi, m[i], m_prim[i], k[i])
+        K4ax = h * f_ax(t[i] + h, x[i] + K3ax, y[i] + K3ay, fi[i] + K3afi, vx[i] + K3vx, vy[i] + K3vy, vfi[i] + K3vfi, m[i], m_prim[i])
+        K4vx = h * f_vx(t[i] + h, x[i] + K3ax, y[i] + K3ay, fi[i] + K3afi, vx[i] + K3vx, vy[i] + K3vy, vfi[i] + K3vfi, m[i], m_prim[i])
+        K4ay = h * f_ay(t[i] + h, x[i] + K3ax, y[i] + K3ay, fi[i] + K3afi, vx[i] + K3vx, vy[i] + K3vy, vfi[i] + K3vfi, m[i], m_prim[i])
+        K4vy = h * f_vy(t[i] + h, x[i] + K3ax, y[i] + K3ay, fi[i] + K3afi, vx[i] + K3vx, vy[i] + K3vy, vfi[i] + K3vfi, m[i], m_prim[i])
+        K4afi = h * f_afi(t[i] + h, x[i] + K3ax, y[i] + K3ay, fi[i] + K3afi, vx[i] + K3vx, vy[i] + K3vy, vfi[i] + K3vfi, m[i], m_prim[i])
+        K4vfi = h * f_vfi(t[i] + h, x[i] + K3ax, y[i] + K3ay, fi[i] + K3afi, vx[i] + K3vx, vy[i] + K3vy, vfi[i] + K3vfi, m[i], m_prim[i])
 
         x.append(x[i] + (K1vx + 2 * K2vx + 2 * K3vx + K4vx) / 6.0)
         vx.append(vx[i] + (K1ax + 2 * K2ax + 2 * K3ax + K4ax) / 6.0)
         y.append(y[i] + (K1vy + 2 * K2vy + 2 * K3vy + K4vy) / 6.0)
         vy.append(vy[i] + (K1ay + 2 * K2ay + 2 * K3ay + K4ay) / 6.0)
-        if vx[i] > 0.1 or vx[i] < - 0.1:
-            fi.append(math.atan2(vy[i], vx[i]))
-        else:
-            fi.append(fi[i])
+        fi.append(fi[i] + (K1vfi + 2 * K2vfi + 2 * K3vfi + K4vfi) / 6.0)
         vfi.append(vfi[i] + (K1afi + 2 * K2afi + 2 * K3afi + K4afi) / 6.0)
 
         #masa , jej pochodna i ciag
-        k.append(f_k(i))
         m.append(f_m(i))
         m_prim.append(f_dm_dt(i))
 
@@ -190,5 +181,3 @@ def animate(i):
 ani = animation.FuncAnimation(fig, animate, len(y),
                               interval=h*100, blit=True, init_func=init)
 plt.show()
-
-print (math.atan2(434.0, 656565.0))
