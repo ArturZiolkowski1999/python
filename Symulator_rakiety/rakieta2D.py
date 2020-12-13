78aad63cf0c71987ceb84fd340947c071640bc7b
ziolko <aaartiii99@gmail.com>
2020-12-13 19:16:11 +0100

    Symulator bez zmienionego fi

--- Symulator_rakiety/rakieta2D.py
+++ Symulator_rakiety/rakieta2D.py
@@ -1,63 +1,80 @@
 from pylab import *
-
+import matplotlib.animation as animation
+import math
 
 # stale
-M0 = 50 #kg masa rakiety
+M0 = 1 #kg masa rakiety bez paliwa
+MP = 10 # masa rakiety z paliwem
 g = 9.81 #m/s**2 przysp ziemskie
-K0 = 300 #m/s**2 ciag silnika poczatkowy
-ak = 0.001 # stala zaniku eksponenty ciagu silnika - model eksponencjalny
-h = 0.01 #s skok
-b = 0 # stala oporow powietrza
+K0 = 30 #m/s**2 ciag silnika poczatkowy
+s = 0.01 # stala zaniku eksponenty ciagu silnika - model eksponencjalny
+c = 3000.0 # v spalin
+h = 0.05 #s skok
+b = 0.2 # stala oporow powietrza
 l = 2 # wysokosc rakiety
-simulation_time = 140 #s czas symulacji
-number_of_steps = 1800 # liczba krokow
+simulation_time = 80 #s czas symulacji
+number_of_steps = 1000 # liczba krokow
+xrange_right = 10000
+xrange_left = -100
+yrange = 8000
 
 
 # funkcja przyspieszenia wspolrzednych uogulnionych z pomoca rownaia lagrangea 2
 def f_ax(t,x,y,fi,vx,vy,vfi, m, m_prim, k):
-    return (m_prim/(2*m)) * l * sin(fi) * vfi + 0.5 *l *cos(fi) * vfi**2 - (m_prim/m) * vx + k * cos(fi) + 0.5 * sin(fi) *l * (-m_prim/(m*(1+0.5*l**2)) * (l*vy*cos(fi)-l*vx*sin(fi)+l**2*vfi+l*sin(fi)*(m_prim/(2*m)*l*sin(fi)*vfi + 0.5*l*cos(fi)*vfi**2 - (m_prim/m) * vx + k *cos(fi)) - l*cos(fi)*(-m_prim/(2*m)*l*cos(fi)*vfi + 0.5*l*sin(fi)*vfi**2 - (m_prim/m) * vy + k * sin(fi)) - 2*k*(x*sin(fi) - y*(cos(fi)))))
+    if y >= 0:
+        return  s * c * (1.0 - (M0 / m)) * cos(fi)
+    else:
+        return 0
 
 def f_ay(t,x,y,fi,vx,vy,vfi, m, m_prim, k):
-    return (m_prim/m) * vy - (m_prim/(2*m)) *l * cos(fi) * vfi + 0.5 *l * sin(fi) * vfi**2 + k*sin(fi) - m *g + (- 0.5)*l * cos(fi)*((-m_prim)/(m*(1+0.5*l**2)) * (l*vy*cos(fi)-l*vx*sin(fi)+l**2*vfi+l*sin(fi)*(m_prim/(2*m)*l*sin(fi)*vfi +0.5*l*cos(fi)*vfi**2 - (m_prim/m) * vx + k *cos(fi)) - l*cos(fi)*(-m_prim/(2*m)*l*cos(fi)*vfi +0.5*l*sin(fi)*vfi**2 - (m_prim/m) * vy + k * sin(fi)) - 2*k*(x*sin(fi) - y*(cos(fi)))))
+    if y >= 0:
+        return -g + s * c * (1.0 - (M0/m)) * sin(fi)
+    else:
+        return 0
 
 def f_afi(t,x,y,fi,vx,vy,vfi,m, m_prim, k):
-    return -m_prim/(m*(1+0.5*l**2)) * (l*vy*cos(fi)-l*vx*sin(fi)+l**2*vfi+l*sin(fi)*(m_prim/(2*m)*l*sin(fi)*vfi +
-            0.5*l*cos(fi)*vfi**2 - (m_prim/m) * vx + k *cos(fi)) - l*cos(fi)*(-m_prim/(2*m)*l*cos(fi)*vfi +
-            0.5*l*sin(fi)*vfi**2 - (m_prim/m) * vy + k * sin(fi)) - 2*k*(x*sin(fi) - y*(cos(fi))))
+    return 0
 
 # funkcja definiujaca przedkosc otrzymana z rownania lagrangea 2
 def f_vx(t,x,y,fi,vx,vy,vfi,m, m_prim, k):
-    return vx
+    if y >= 0:
+        return vx
+    else:
+        return 0
 
 def f_vy(t,x,y,fi,vx,vy,vfi,m, m_prim, k):
-    return vy
+    if y >= 0:
+        return vy
+    else:
+        return 0
 
 def f_vfi(t,x,y,fi,vx,vy,vfi, m, m_prim, k):
     return vfi
 
 # funkcje do wyliczania masy i jej pochodnej oraz ciagu
-def f_k(t):
-    return K0 * exp(-ak * t)
 
 def f_m(t):
-    return (M0-10) * exp(-ak * t) + 10
+    return M0 + MP * exp(-s * t)
 
 def f_dm_dt(t):
-    return M0 * (-ak) * exp(-ak * t) + 10 * ak * exp(-ak * t)
+    return -s*(MP * exp(-s * t))
+
+def f_k(t):
+    return 0
 
 
 
 # warunki poczatkowe
 x0 = 0 #m polozenie poczatkowe
 vx0 = 0 #m/s predkosc poczatkowa
-y0 = 10
-vy0 = 4440
-fi0 = pi/2
+y0 = 0
+vy0 = 0
+fi0 = pi/4
 vfi0 = 0
 
 
 #tablice z obliczonymi danymi
-t = linspace(0, simulation_time, number_of_steps) #s czas od startu do 40 sekundy, 1000 krokow
+t = np.arange(0, simulation_time, h)
 x = [x0] #tablica polozen x
 vx = [vx0] #tablica predkosci x
 y = [y0]
@@ -66,7 +83,7 @@ fi = [fi0]
 vfi = [vfi0]
 #definicja ciagu i masy
 k = [K0]
-m = [M0]
+m = [M0 + MP]
 m_prim = [f_dm_dt(0)]
 
 
@@ -108,14 +125,17 @@ for i in range(len(t) - 1):
         vx.append(vx[i] + (K1ax + 2 * K2ax + 2 * K3ax + K4ax) / 6.0)
         y.append(y[i] + (K1vy + 2 * K2vy + 2 * K3vy + K4vy) / 6.0)
         vy.append(vy[i] + (K1ay + 2 * K2ay + 2 * K3ay + K4ay) / 6.0)
-        fi.append(fi[i] + (K1vfi + 2 * K2vfi + 2 * K3vfi + K4vfi) / 6.0)
+        if vx[i] > 0.1 or vx[i] < - 0.1:
+            fi.append(math.atan2(vy[i], vx[i]))
+        else:
+            fi.append(fi[i])
         vfi.append(vfi[i] + (K1afi + 2 * K2afi + 2 * K3afi + K4afi) / 6.0)
 
         #masa , jej pochodna i ciag
         k.append(f_k(i))
         m.append(f_m(i))
         m_prim.append(f_dm_dt(i))
-   #
+
 
 subplot(2, 4, 1)
 plot(t, y)
@@ -136,9 +156,39 @@ subplot(2, 4, 7)
 plot(t, vfi)
 title("vfi(t)")
 subplot(2, 4, 4)
-plot(t, k)
-title("k(t)")
+plot(t, m_prim)
+title("m'(t)")
 subplot(2, 4, 8)
 plot(t, m)
 title("m(t)")
-show()
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111, autoscale_on=False, xlim=(xrange_left, xrange_right), ylim=(-1, yrange))
+ax.set_aspect('equal')
+ax.grid()
+
+line, = ax.plot([], 'x-', lw=2)
+time_template = 'time = %.1fs'
+time_text = ax.text(0.05, 0.9, '', transform=ax.transAxes)
+
+
+def init():
+    line.set_data([], [])
+    time_text.set_text('')
+    return line, time_text
+
+
+def animate(i):
+    thisx = [x[i]]
+    thisy = [y[i]]
+    line.set_data(thisx, thisy)
+    time_text.set_text(time_template % (i*h))
+    return line, time_text
+
+
+ani = animation.FuncAnimation(fig, animate, len(y),
+                              interval=h*100, blit=True, init_func=init)
+plt.show()
+
+print (math.atan2(434.0, 656565.0))
--- Symulator_rakiety/rocket2D_animation.py
+++ Symulator_rakiety/rocket2D_animation.py
@@ -7,11 +7,14 @@ from pylab import *
 
 # Stałe początkowe
 G = 9.8  # acceleration due to gravity, in m/s^2
-M = 1.0  # mass of pendulum 1 in kg
+MP = 10.0  #kg mass of paliwo
+M0 = 5.0 #kg masa rakiety bez paliwa
 L = 2.5 # lengh of... in m
 b = 0.1 # stala oporow powietrza
-K0 = 100 #m/s**2 ciag silnika poczatkowy
-ak = 0.002 # stala zaniku eksponenty ciagu silnika - model eksponencjalny
+s = 0.02 # stala zaniku eksponenty ciagu silnika - model eksponencjalny
+c = 3000.0 #m/s predkosc wyrzutu paliwa
+simulation_time = 140 #s czas symulacji
+dt = 0.05 #s krok czasowy
 
 # definicje równań różniczkowych
 def derivs(state, t):
@@ -20,39 +23,37 @@ def derivs(state, t):
     # x  0
     dydx[0] = state[1]
     # vx  1
-    dydx[1] = 0
+    dydx[1] = 0 #2*s*c*(1.0 - (M0/state[7])) * sin(state[4])
     # y  2
     dydx[2] = state[3]
     # vy  3
-    dydx[3] = -G + K0 * exp(-ak * t**3) - b * state[3]
+    dydx[3] = -G + 2 * s * c * (1.0 - (M0/(-2 * s * (state[6]) - M0))) #* cos(state[4])
     # fi  4
     dydx[4] = state[5]
     # vfi  5
-    dydx[5] = -L*G* cos(state[4])
+    dydx[5] = 0
     # m  6
-    dydx[6] = 2
-    # dm/dt  7
-    dydx[7] = 0
-    # k   8
-    dydx[8] = 5
+    dydx[6] = M0 + MP * (exp(-s*t**2))
+
+
 
     return dydx
 
 # tworzenie kroku i tablicy czasu
-dt = 0.05
-t = np.arange(0, 20, dt)
+
+t = np.arange(0, simulation_time, dt)
 
 # Początkowe warunki
 y0 = L
 vy0 = 0.0
 x0 = 1000.0
 vx0 = 0.0
-fi0 = 0
+fi0 = pi/2
 vfi0 = 0.0
 
 
 # stan początkowy w tabeli
-state = [x0, vx0, y0, vy0, fi0, vfi0]
+state = [x0, vx0, y0, vy0, fi0, vfi0, M0+MP, ]
 
 # całkowanie prędkośći by otrzymać kąt
 y = integrate.odeint(derivs, state, t)
@@ -65,7 +66,7 @@ x1 = y[:, 0]
 fi1 = y[:, 4]
 # plotowanie animacji
 fig = plt.figure()
-ax = fig.add_subplot(111, autoscale_on=False, xlim=(-1, 6000), ylim=(-1, 6000))
+ax = fig.add_subplot(111, autoscale_on=False, xlim=(-10, 60), ylim=(-10, 60))
 ax.set_aspect('equal')
 ax.grid()
 
@@ -88,6 +89,6 @@ def animate(i):
     return line, time_text
 
 
-ani = animation.FuncAnimation(fig, animate, range(1, len(y)),
+ani = animation.FuncAnimation(fig, animate, len(y),
                               interval=dt*100, blit=True, init_func=init)
 plt.show()
